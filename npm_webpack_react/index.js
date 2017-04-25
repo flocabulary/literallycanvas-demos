599e707d872af652c6676571351c746672512625
Greg Altman <greg@flocabulary.com>
2017-04-24 20:09:09 -0400

    futz with literally canvas options and css

--- npm_webpack_react/index.js
+++ npm_webpack_react/index.js
@@ -5,7 +5,23 @@ const LC = require('literallycanvas');
 
 const MyApp = React.createClass({
     render: function() {
-        return <LC.LiterallyCanvasReactComponent imageURLPrefix="/build/lc-assets/img" />;
+        return (
+            <LC.LiterallyCanvasReactComponent
+                imageURLPrefix="/build/lc-assets/img"
+                tools={
+                    [
+                      LC.tools.Pencil,
+                      LC.tools.Eraser,
+                      LC.tools.Line,
+                      LC.tools.Rectangle,
+                      LC.tools.Text
+                    ]
+                }
+                zoomMax={1}
+                zoomMin={1}
+
+            />
+        );
     }
 });
 
